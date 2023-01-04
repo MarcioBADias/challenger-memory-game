7b4ed91e40f3587b55f89e298d611e39410aa3d0
MarcioBADias <98918078+MarcioBADias@users.noreply.github.com>
2023-01-04 11:47:57 -0300

    Ajustando a seleção dos itens com JS

--- js/appIndex.js
+++ js/appIndex.js
@@ -1,10 +1,21 @@
 const form = document.querySelector('form');
+const buttonsTheme = document.querySelectorAll('.theme')
+const buttonsNumberOfPlayers = document.querySelectorAll('.btn-players')
 
+
+const selectItem = (area, target) => {
+     area.forEach((button) => {
+          if(button.classList.contains('clicked')){
+               button.classList.remove('clicked')
+          }
+     })
+     target.classList.add('clicked');
+}
 form.addEventListener('click', e => {
-    e.preventDefault();
-
-   if(e.target.classList.contains('btn')){
-        e.target.classList.add('clicked');
-   }
-
+     e.preventDefault();
+     const target = e.target;
+     if(e.target.classList.contains('theme')){
+          
+          selectItem(buttonsNumberOfPlayers,target);
+     }
 })
